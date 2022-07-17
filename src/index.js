be3cd414e9f94c7784fa70b01081ef7053942667
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-07-17 22:36:45 +0300

    зробив рендер кнопок пагінації

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
 import FilmsApiService from './js/fetch-api';
 import { cardMurkup } from './js/cardMurkup';
+import pagination from './js/pagination';
 
 const filmsApi = new FilmsApiService();
 
 const refs = {
-    searchForm: document.querySelector('.header-form'),
-    searchButton: document.querySelector('.header-form__btn'),
-    filmCard: document.querySelector('.gallery'),
-}
+  searchForm: document.querySelector('.header-form'),
+  searchButton: document.querySelector('.header-form__btn'),
+  filmCard: document.querySelector('.gallery'),
+};
 
-refs.searchForm.addEventListener('submit', onSearch);
+const ulTag = document.querySelector('ul');
+// console.log(pagination);
 
+filmsApi.fetchTrendingFilms().then(r => {
+  console.log(r);
+  const page = r.page;
+  const totalPages = r.total_pages;
+  //   console.log(page, totalPages);
+  pagination(totalPages, page);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// refs.searchForm.addEventListener('submit', onSearch);
 
 // function onSearch(event) {
 //     event.preventDefault();
@@ -41,11 +29,10 @@ refs.searchForm.addEventListener('submit', onSearch);
 //     filmsApi.query = event.currentTarget.elements.userSearchQuery.value;
 //     if (filmsApi.query === '') {
 //         return alert('пустая строка');
-//     } 
+//     }
 //     filmsApi.fetchFilmsOnSearch().then(data => {
 //         console.log(data);
 //         refs.filmCard.insertAdjacentHTML('beforeend', cardMurkup(data.results));
-//     });   
-    
-    
+//     });
+
 // }
