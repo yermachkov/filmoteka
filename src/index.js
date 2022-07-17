fad3c3bf8c261fa83ae6b26efba85904eeb32303
olhazolotar <olchere@gmail.com>
2022-07-17 12:56:11 +0100

    add cardMurkup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,51 @@
 import FilmsApiService from './js/fetch-api';
+import { cardMurkup } from './js/cardMurkup';
 
 const filmsApi = new FilmsApiService();
 
+const refs = {
+    searchForm: document.querySelector('.header-form'),
+    searchButton: document.querySelector('.header-form__btn'),
+    filmCard: document.querySelector('.gallery'),
+}
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onSearch(event) {
+//     event.preventDefault();
+
+//     filmsApi.query = event.currentTarget.elements.userSearchQuery.value;
+//     if (filmsApi.query === '') {
+//         return alert('пустая строка');
+//     } 
+//     filmsApi.fetchFilmsOnSearch().then(data => {
+//         console.log(data);
+//         refs.filmCard.insertAdjacentHTML('beforeend', cardMurkup(data.results));
+//     });   
+    
+    
+// }
