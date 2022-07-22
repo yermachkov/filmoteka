6f85d4e7266a9b16e41015ad508019c4970a5a1a
Eldiwr <Eldiwr97@gmail.com>
2022-07-22 18:22:06 +0300

    Modal animatian +svg
    Сделал полностью анимацию и оформление модалки в футере.

--- src/js/team-data.js
+++ src/js/team-data.js
@@ -47,6 +47,54 @@ const teamDAta = [
     git: 'https://github.com/Eldiwr',
     linked: 'https://www.linkedin.com/in/maksym-kidanov-53b08b21a/',
     insta: 'https://www.instagram.com/eldiwr/',
-},
+    },
+    
+    {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
+    alt: "кто-то тут есть",
+  
+    name: 'Maksym Kidanov',
+    pos: "Developer",
+
+    git: 'https://github.com/Eldiwr',
+    linked: 'https://www.linkedin.com/in/maksym-kidanov-53b08b21a/',
+    insta: 'https://www.instagram.com/eldiwr/',
+    },
+
+    {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
+    alt: "кто-то тут есть",
+  
+    name: 'Maksym Kidanov',
+    pos: "Developer",
+
+    git: 'https://github.com/Eldiwr',
+    linked: 'https://www.linkedin.com/in/maksym-kidanov-53b08b21a/',
+    insta: 'https://www.instagram.com/eldiwr/',
+    },
+
+    {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
+    alt: "кто-то тут есть",
+  
+    name: 'Maksym Kidanov',
+    pos: "Developer",
+
+    git: 'https://github.com/Eldiwr',
+    linked: 'https://www.linkedin.com/in/maksym-kidanov-53b08b21a/',
+    insta: 'https://www.instagram.com/eldiwr/',
+    },
+
+    {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
+    alt: "кто-то тут есть",
+  
+    name: 'Maksym Kidanov',
+    pos: "Developer",
+
+    git: 'https://github.com/Eldiwr',
+    linked: 'https://www.linkedin.com/in/maksym-kidanov-53b08b21a/',
+    insta: 'https://www.instagram.com/eldiwr/',
+    },
     
 ]
