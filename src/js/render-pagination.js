a5baa9e9402216143e6b84b8df9d2a40fe200f0d
Alexey Yermachkov <o.yermachkov@gmail.com>
2022-07-22 20:10:11 +0300

    makes some order, including pagination

---
+++ src/js/render-pagination.js
@@ -0,0 +1,69 @@
+import { refs } from './refs';
+import pagination from './pagination';
+import './paginationBtnWorks';
+import FilmsApiService from './fetch-api';
+import { renderGallery } from './templates/render-gallery';
+
+
+
+const filmsApi = new FilmsApiService();
+
+filmsApi.fetchTrendingFilms().then(r => {
+  console.log(r);
+  const page = r.page;
+  const totalPages = r.total_pages;
+
+  renderGallery(r.results);
+  pagination(totalPages, page);
+});
+
+const ulTag = document.querySelector('.pagination ul');
+
+ulTag.addEventListener('click', onClick);
+
+function onClick(evt) {
+  refs.gallery.innerHTML = '';
+  console.log(evt.target);
+
+  if (evt.target === document.querySelector('.arrow-left')) {
+    filmsApi.pageDecrement();
+    filmsApi.fetchTrendingFilms().then(r => {
+      console.log(r);
+      const page = r.page;
+      const totalPages = r.total_pages;
+
+      console.log(evt.target);
+      renderGallery(r.results);
+      pagination(totalPages, page);
+      console.log(Number(document.querySelector('.numb').textContent));
+    });
+  }
+
+  const allNumbes = document.querySelectorAll('.numb');
+  for (const number of allNumbes) {
+    if (evt.target === number) {
+      filmsApi.setPage(Number(evt.target.textContent));
+      filmsApi.fetchTrendingFilms().then(r => {
+        console.log(r);
+        const page = r.page;
+        const totalPages = r.total_pages;
+
+        console.log(evt.target);
+        renderGallery(r.results);
+        pagination(totalPages, page);
+      });
+    }
+  }
+
+  if (evt.target === document.querySelector('.arrow-right')) {
+    filmsApi.pageIncrement();
+    filmsApi.fetchTrendingFilms().then(r => {
+      console.log(r);
+      const page = r.page;
+      const totalPages = r.total_pages;
+
+      renderGallery(r.results);
+      pagination(totalPages, page);
+    });
+  }
+};
