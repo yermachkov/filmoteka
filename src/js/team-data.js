f2650218947af98b15aab6aca08f879fe4a2d96d
Valeriy Semko <valsemko74@gmail.com>
2022-07-24 15:39:11 +0300

    add photo and link

--- src/js/team-data.js
+++ src/js/team-data.js
@@ -2,15 +2,15 @@ export {teamDAta}
 
 const teamDAta = [
     {
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
-    alt: "кто-то тут есть",
+    img: '../images/teams-photo/0-02-05-6ae652b9a9208aaabb65a472036d85a105d18b87d6b382b8e29ca21ae03fcd35_1c6daaf6dfb553.jpg',
+    alt: "Valeriy Semko photo",
   
-    name: 'Maksym Kidanov',
-    pos: "Developer",
+    name: 'Valeriy Semko',
+    pos: "Team Lead",
 
-    git: 'https://github.com/Eldiwr',
-    linked: 'https://www.linkedin.com/in/maksym-kidanov-53b08b21a/',
-    insta: 'https://www.instagram.com/eldiwr/',
+    git: 'https://github.com/Ussuriec74',
+    linked: 'https://www.linkedin.com/in/semko-val/',
+    insta: 'https://www.instagram.com/valeriysemko/',
     },
 
     {
