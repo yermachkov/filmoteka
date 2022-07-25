ded28576397ea0b5e09b58e8014fcbec7a63942c
Dmitry Shushaiev <shushikian@gmail.com>
2022-07-25 23:41:50 +0300

    Update team-data.js

--- src/js/team-data.js
+++ src/js/team-data.js
@@ -14,15 +14,15 @@ const teamDAta = [
     },
 
     {
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
-    alt: "кто-то тут есть",
+    img: 'https://scontent.fiev15-1.fna.fbcdn.net/v/t1.18169-9/1797999_700031970046846_550160208_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=z8Q_cSSAxAwAX_N4zMa&_nc_ht=scontent.fiev15-1.fna&oh=00_AT8XLRqjGVJPx2ogjrOud8LuICiLQ7-6yyMN5ZAVI77ghQ&oe=6305142C',
+    alt: "Dima Shushaiev photo",
   
-    name: 'Maksym Kidanov',
-    pos: "Developer",
+    name: 'Dima Shushaiev',
+    pos: "Scrum Master",
 
-    git: 'https://github.com/Eldiwr',
-    linked: 'https://www.linkedin.com/in/maksym-kidanov-53b08b21a/',
-    insta: 'https://www.instagram.com/eldiwr/',
+    git: 'https://github.com/Shushikian',
+    linked: 'https://www.linkedin.com/in/dimitryi-shushaiev-238916173/',
+    insta: 'https://www.instagram.com/shyshikian/',
     },
 
     {
