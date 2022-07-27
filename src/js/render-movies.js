0c1b4fe8a9822a2eaf12e9ee4d5bda0fd8b6394d
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-07-27 14:11:52 +0300

    збереження сторінки

--- src/js/render-movies.js
+++ src/js/render-movies.js
@@ -32,6 +32,7 @@ function onClick(evt) {
 
   if (evt.target === arrowLeft) {
     filmsApi.pageDecrement();
+    localStorage.setItem('page', JSON.stringify(filmsApi.getPage()));
     if (filmsApi.searchQuery !== null && filmsApi.searchQuery !== '') {
       search();
     } else popular();
@@ -40,6 +41,7 @@ function onClick(evt) {
   for (const number of allNumbes) {
     if (evt.target === number) {
       filmsApi.setPage(Number(evt.target.textContent));
+      localStorage.setItem('page', JSON.stringify(filmsApi.getPage()));
       if (filmsApi.searchQuery !== null && filmsApi.searchQuery !== '') {
         search();
       } else popular();
@@ -48,6 +50,7 @@ function onClick(evt) {
 
   if (evt.target === arrowRight) {
     filmsApi.pageIncrement();
+    localStorage.setItem('page', JSON.stringify(filmsApi.getPage()));
     if (filmsApi.searchQuery !== null && filmsApi.searchQuery !== '') {
       search();
     } else popular();
@@ -73,14 +76,25 @@ function onSearchSubmit(event) {
     return;
   }
 
+  // filmsApi.setPage(JSON.parse(localStorage.getItem('page')));
+
   filmsApi.fetchFilmsOnSearch().then(response => {
     if (response.total_results === 0) {
       let y = JSON.parse(localStorage.getItem('respon'));
       console.log(y);
       const z = y.length - 2;
       console.log(y[z]);
+
+      if (y[z] === undefined) {
+        popular();
+        filmsApi.query = '';
+        refs.searchError.classList.remove('is-hidden');
+        return;
+      }
+      filmsApi.setPage(JSON.parse(localStorage.getItem('page')));
       filmsApi.query = y[z];
       request.pop();
+
       search();
       refs.searchError.classList.remove('is-hidden');
       return;
@@ -95,6 +109,7 @@ function onSearchSubmit(event) {
 }
 
 function paginationRenderBtn(r) {
+  console.log(r);
   const page = r.page;
   const totalPages = r.total_pages;
   renderGallery(r.results);
